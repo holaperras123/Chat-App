92af9354856835291387266b6724c990df8bf71f
holaperras123 <simonvelasquezidk@gmail.com>
2024-06-27 15:45:49 -0500

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -9,8 +10,9 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
-
 const PORT = process.env.PORT || 4653;
+
+const __dirname = path.resolve()
 
 dotenv.config();
 
@@ -21,13 +23,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
 
-// app.get("/", (req,res) => {
-    // root route http://localhost:4653/
-    // res.send("Hello World!!");
-// })
-
-
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 server.listen(PORT, () => {
     connectToMongoDB()
